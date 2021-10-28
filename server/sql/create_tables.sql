f4289c96cefb2870284c6b97d84b9ffddb8b7b5f
Emmanuel Torres <emmanuel.torresamaya@gmail.com>
2021-10-27 20:36:38 -0600

    Fixed problem with compose and inti scripts for the db

--- server/sql/create_tables.sql
+++ server/sql/create_tables.sql
@@ -17,14 +17,3 @@ CREATE TABLE IF NOT EXISTS comment (
     posted_on TIMESTAMP,
     CONSTRAINT fk_post FOREIGN KEY (post_id) REFERENCES post(post_id)
 );
-
-INSERT INTO post(title, author, content, posted_on)
-    VALUES ('Sample Title 1', 'Sample Author 1', 'Sample Contnet 1',  NOW()),
-           ('Sample Title 2', 'Sample Author 2', 'Sample Contnet 2',  NOW()),
-           ('Sample Title 3', 'Sample Author 3', 'Sample Contnet 3',  NOW());
-
-INSERT INTO comment(post_id, content, author, posted_on)
-    VALUES (1, 'This is sample comment #1', 'Sample Author 1', NOW()),
-           (1, 'This is sample comment #2', 'Sample Author 1', NOW()),
-           (2, 'This is sample comment #3', 'Sample Author 2', NOW()),
-           (3, 'This is sample comment #4', 'Sample Author 3', NOW());
--- server/sql/fill_tables.sql
+++ server/sql/fill_tables.sql
@@ -3,3 +3,9 @@ INSERT INTO post(title, author, content, posted_on)
            ('Sample Title 2', 'Sample Author 2', 'Sample Contnet 2',  NOW()),
            ('Sample Title 3', 'Sample Author 3', 'Sample Contnet 3',  NOW()),
            ('Sample Title 4', 'Sample Author 4', 'Sample Contnet 4',  NOW());
+
+INSERT INTO comment(post_id, content, author, posted_on)
+    VALUES (1, 'This is sample comment #1', 'Sample Author 1', NOW()),
+           (1, 'This is sample comment #2', 'Sample Author 1', NOW()),
+           (2, 'This is sample comment #3', 'Sample Author 2', NOW()),
+           (3, 'This is sample comment #4', 'Sample Author 3', NOW());
